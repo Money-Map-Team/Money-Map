0a5183a1679486107c3387d40fb9cbc5f4acb3da
okjazim <jazimjunaith02@gmail.com>
2025-01-18 17:52:46 +0100

    added alpha-1 code

--- backend/transaction_handler.py
+++ backend/transaction_handler.py
@@ -1,7 +1,24 @@
-from user_storage import user_storage
+from backend.user_storage import UserStorage
 
-class transaction_handler:
+class TransactionHandler:
     def __init__(self):
-        print("transaction_handler initialized")
+        self.storage = UserStorage(filepath="userdata/transactions.json")  
 
-#In-Progress
+    def add_transaction(self, amount, category, description):
+        transaction = {
+            'amount': f"€{amount:.2f}",
+            'category': category,
+            'description': description
+        }
+        self.storage.save_transaction(transaction)
+
+    def remove_transaction(self, index):
+        try:
+            transactions = self.storage.readfile()['transactions']
+            removed_transaction = transactions.pop(index)
+            self.storage.remove_transaction(removed_transaction)
+        except IndexError:
+            raise ValueError("Invalid index. Transaction not found.")
+
+    def view_transactions(self):
+        return self.storage.readfile()['transactions']
