47ea986a45c14c2863836865f908ffcfc01b9920
okjazim <jazimjunaith02@gmail.com>
2025-01-18 17:53:12 +0100

    added alpha-1 code

--- backend/user_storage.py
+++ backend/user_storage.py
@@ -1,14 +1,33 @@
 import os
 import json
+from pathlib import Path
 
-class user_storage:
-    
-    def readfile(filepath):
-        if os.path.exists(filepath):
-            with open(filepath, 'r') as file:
+class UserStorage:
+    def __init__(self, filepath="userdata/transactions.json"):
+        self.filepath = os.path.join(Path(__file__).parent.parent, filepath)
+        if not os.path.exists(self.filepath):
+            raise FileNotFoundError(f"{self.filepath} does not exist. Please make sure the file is present.")
+    def readfile(self):
+        with open(self.filepath, 'r') as file:
+            try:
                 return json.load(file)
-        
-    def writefile(filepath,data):
-        if os.path.exists(filepath):
-            with open(filepath, 'w') as file:
+            except json.JSONDecodeError:
+                return {"transactions": []}
+    
+    def writefile(self, data):
+        try:
+            with open(self.filepath, 'w') as file:
                 json.dump(data, file, indent=4)
+        except IOError as e:
+            print(f"Error writing to file: {e}")
+
+    def save_transaction(self, transaction):
+        data = self.readfile()
+        data['transactions'].append(transaction)
+        self.writefile(data)
+
+    def remove_transaction(self, transaction):
+        data = self.readfile()
+        if transaction in data['transactions']:
+            data['transactions'].remove(transaction)
+            self.writefile(data)
