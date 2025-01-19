53dc57d7d04c2fcddde14b2df6082ba77e28559d
RyanSims67 <rsim6115@gmail.com>
2025-01-19 07:28:20 +0100

    Test

--- backend/report_gen.py
+++ backend/report_gen.py
@@ -1,1 +1,13 @@
 #In-Progress
+class report_gen:
+    def __init__(self):
+        self.budget = {}
+
+    def update_budget(self, month, amount):
+        self.budget[month] = amount
+        self.show_changes()
+
+    def show_changes(self):
+        print("Updated Budget:")
+        for month, amount in self.budget.items():
+            print(f"{month}: ${amount}")
